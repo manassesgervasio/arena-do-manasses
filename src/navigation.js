944b94c41fd1f6090fec13144d071f3ee6df86de
Manasses Gervasio <manassesgervasio@hotmail.com>
2026-05-21 00:38:24 -0300

    Cria base de paginas e navegacao

---
+++ src/navigation.js
@@ -0,0 +1,17 @@
+export const navigationItems = [
+  {
+    id: "agenda",
+    label: "Agenda",
+    path: "/agenda",
+  },
+  {
+    id: "clientes",
+    label: "Clientes",
+    path: "/clientes",
+  },
+  {
+    id: "financeiro",
+    label: "Financeiro",
+    path: "/financeiro",
+  },
+];
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Agenda } from "./Agenda";
+export { default as Clientes } from "./Clientes";
+export { default as Financeiro } from "./Financeiro";
+export { default as Home } from "./Home";
